f4a876d3112152920e301ef02504bf4cca42d8e3
Thomas_Johnson <tjohnson1106@gmail.com>
2018-02-23 16:51:47 -0500

    accessed and manipulated endpoint

--- testService/handler.js
+++ testService/handler.js
@@ -1,12 +1,12 @@
-'use strict';
+"use strict";
 
 module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+      message: "Go Serverless v1.0! Your function executed beautifully!",
+      input: event
+    })
   };
 
   callback(null, response);
